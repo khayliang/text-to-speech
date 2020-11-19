2df726fd6d60dc40984ea2e1c2aa8373afa7011e
khayliang <khayliang@gmail.com>
2020-11-19 21:48:44 +0800

    fixed bug where event.body is empty

--- lambdas/create-speech-api.js
+++ lambdas/create-speech-api.js
@@ -2,12 +2,14 @@ const Responses = require("../common/api-responses")
 const Sns = require("../common/sns")
 
 exports.handler = async (event, ctx) => {
-  if (!event.body.text) {
+  const body = JSON.parse(event.body)
+
+  if (!body.text) {
     return Responses._400({ 
       message: 'missing speech text' 
     });
   }  
-  const text = event.body.text;
+  const text = body.text;
   try{
       speech_info = await Sns.sendSpeechTopic(text)
       return Responses._200({
